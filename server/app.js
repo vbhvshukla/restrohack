fa34623bc3cae7c55307b0a6b1b3460b5b930228
Vaibhav <rushukla7@gmail.com>
2025-04-11 05:08:58 +0530

    Added Feedback Routes

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { userRoutes, teamRoutes, departmentRoutes, positionRoutes, questionnaireRoutes, authRoutes } from "./routes/imports.routes.js";
+import { userRoutes, teamRoutes, departmentRoutes, positionRoutes, questionnaireRoutes, authRoutes, feedbackRoutes } from "./routes/imports.routes.js";
 
 const app = express();
 const API_PREFIX = process.env.API_PREFIX || '/api/v1';
 
 //CORS configuration
 app.use(cors({
-    origin: ["http://127.0.0.1:5500","http://127.0.0.1:5173","http://localhost:5173"],
+    origin: ["http://127.0.0.1:5500", "http://127.0.0.1:5173", "http://localhost:5173"],
     credentials: true
 }));
 
@@ -28,4 +28,6 @@ app.use(`${API_PREFIX}/team`, teamRoutes);
 app.use(`${API_PREFIX}/user`, userRoutes);
 app.use(`${API_PREFIX}/questionnaire`, questionnaireRoutes);
 app.use(`${API_PREFIX}/auth`, authRoutes);
+app.use(`${API_PREFIX}/feedback`, feedbackRoutes);
+
 export { app }
