1a6ff2a969a14e1ba32e3aad484eb4aaed6232e8
Vaibhav <rushukla7@gmail.com>
2025-04-11 04:02:22 +0530

    Updated all indexes

--- server/model/position.model.js
+++ server/model/position.model.js
@@ -19,9 +19,5 @@ const positionSchema = new mongoose.Schema({
   timestamps: true
 });
 
-// Add index for better query performance
-positionSchema.index({ level: 1 });
-positionSchema.index({ name: 1 });
-
 const Position = mongoose.model("Position", positionSchema);
 export { Position, positionSchema };
