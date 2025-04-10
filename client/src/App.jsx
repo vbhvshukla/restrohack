9a50b35ac36fb2c680b7140e1e3846ad3a2b1281
AMAN-GUPTA29 <aman.g21@iiits.in>
2025-04-11 01:09:22 +0530

    uiw

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Login from './components/Login';
 import OrganizationStructure from './components/OrganizationStructure';
 import TeamCreation from './components/TeamCreation';
 import TeamViewer from './components/TeamViewer';
+import PositionForm from './components/PositionForm';
 
 function App() {
 	return (
@@ -12,6 +13,7 @@ function App() {
 				<Route path="/login" element={<Login />} />
 				<Route path="/teams/create" element={<TeamCreation />} />
 				<Route path="/teams/view" element={<TeamViewer />} />
+				<Route path="/positions/add" element={<PositionForm />} />
 			</Routes>
 		</Router>
 	);
